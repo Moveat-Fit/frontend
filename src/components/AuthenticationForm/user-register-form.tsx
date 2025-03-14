0ae22959e1e2e1af06f5adf429a9f5f55afbae95
Natã Pedro <natanpedrodasilva@gmail.com>
2025-03-14 20:31:22 -0300

    feat: creating password validation

--- src/components/AuthenticationForm/user-register-form.tsx
+++ src/components/AuthenticationForm/user-register-form.tsx
@@ -8,13 +8,14 @@ import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Loader } from "lucide-react"
+import { CheckCircle, CheckCircle2, Eye, EyeOff, Loader, XCircle } from "lucide-react"
 import { registerService } from '../../services/authService';
 import Link from "next/link"
-import { useState } from "react"
+import { use, useEffect, useState } from "react"
 import Router from 'next/router';
 import { Checkbox } from '../ui/checkbox';
 import ToastError from '../ToastError';
+import { set } from 'react-hook-form';
 
 type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>
 
@@ -26,32 +27,62 @@ export function UserRegisterAuthForm({ className, ...props }: UserAuthFormProps)
     const [occupationDocument, setOccupationDocument] = useState<string>("");
     const [cellphone, setCellphone] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null);
     const [success, setSuccess] = useState<string | null>(null);
+    const [canSubmit, setCanSubmit] = useState<boolean>(false);
+
+    const [passwordVisible, setPasswordVisible] = useState(false); // Estado para controlar a visibilidade da senha
+    const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false); // Estado para confirmar a senha
+
+    // Função para alternar a visibilidade da senha
+    const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
+    const toggleConfirmPasswordVisibility = () => setConfirmPasswordVisible(!confirmPasswordVisible);
+
+
+    const criteria = [
+        { text: "Senha precisa ter no mínimo 8 dígitos", test: (pwd: string) => pwd.length >= 8 },
+        { text: "Senha precisa ter caracteres especiais", test: (pwd: string) => /[!@#$%^&*(),.?":{}|<>]/.test(pwd) },
+        { text: "Senha precisa ter letras maiúsculas e minúsculas", test: (pwd: string) => /[a-z]/.test(pwd) && /[A-Z]/.test(pwd) }
+    ];
+
+    const nameIsValid = (name: string) => {
+        return (
+            name.trim().length >= 3 &&
+            name.trim().length <= 50 &&
+            /^[A-Za-zÀ-ÿ'\s]+$/.test(name)
+        );
+    };
+
+
+    const allValid = nameIsValid(name) && criteria.every(({ test }) => test(password)) && password === confirmPassword && password.length > 0;
+
+    const handleSubmit = () => {
+        if (!nameIsValid(name)) {
+            ToastError({ message: "O nome deve ter entre 3 e 50 caracteres e conter apenas letras e espaços." });
+            return;
+        }
+    };
 
     const formatCPF = (value: string) => {
-        // Remove tudo que não for número
         const cleaned = value.replace(/\D/g, "");
 
-        // Aplica a máscara: 000.000.000-00
         return cleaned
             .replace(/^(\d{3})(\d)/, "$1.$2")
             .replace(/^(\d{3})\.(\d{3})(\d)/, "$1.$2.$3")
             .replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4")
-            .slice(0, 14); // Garante o tamanho correto
+            .slice(0, 14);
     };
 
     const formatPhone = (value: string) => {
-        // Remove tudo que não for número
         const cleaned = value.replace(/\D/g, "");
 
-        // Aplica a máscara para telefone: (00) 00000-0000
         return cleaned
-            .replace(/(\d{2})(\d)/, "($1) $2") // Adiciona o DDD com parênteses
-            .replace(/(\d)(\d{4})$/, "$1-$2") // Adiciona o hífen após o 5º dígito
-            .slice(0, 15); // Garante que o número não ultrapasse 15 caracteres
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{5})(\d{4})/, '$1-$2')
+            .slice(0, 15);
     };
 
     const handleChangeCellphone = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,24 +96,22 @@ export function UserRegisterAuthForm({ className, ...props }: UserAuthFormProps)
 
     const handleSelect = (value: string) => setUserType(value);
 
+
+
+    useEffect(() => {
+        if (userType != "Paciente" && occupationDocument.length < 6) setCanSubmit(false);
+        if (cpf.length < 14) setCanSubmit(false);
+        if (cellphone.length < 15) setCanSubmit(false);
+
+    }, [cellphone.length, cpf.length, occupationDocument, userType]);
+
+
     const onSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         setIsLoading(true);
         setError(null);
         setSuccess(null);
-
-        if (cpf.length < 14) {
-            ToastError({ message: "CPF inválido" });
-            setIsLoading(false);
-            return;
-        }
-
-        if (cellphone.length < 15) {
-            ToastError({ message: "Telefone inválido" });
-            setIsLoading(false);
-            return;
-        }
 
         try {
             await registerService({
@@ -94,7 +123,6 @@ export function UserRegisterAuthForm({ className, ...props }: UserAuthFormProps)
                 user_type: userType,
             });
             setSuccess("Cadastro realizado com sucesso!");
-
 
             toast.success('Cadastro realizado com sucesso', {
                 position: "top-center",
@@ -193,7 +221,9 @@ export function UserRegisterAuthForm({ className, ...props }: UserAuthFormProps)
                             autoComplete="email"
                             autoCorrect="off"
                             disabled={isLoading}
+
                         />
+
                         <div className="flex gap-2">
                             <Input
                                 required
@@ -218,21 +248,79 @@ export function UserRegisterAuthForm({ className, ...props }: UserAuthFormProps)
                                 disabled={isLoading}
                             />
                         </div>
-                        <Input
-                            required
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            id="password"
-                            placeholder="Senha"
-                            type="password"
-                            autoCapitalize="none"
-                            autoComplete="new-password"
-                            autoCorrect="off"
-                            disabled={isLoading}
-                        />
+                        <div className='relative'>
+                            <Input
+                                required
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                id="password"
+                                placeholder="Senha"
+                                type={passwordVisible ? "text" : "password"} // Condicionando o tipo de input
+                                autoCapitalize="none"
+                                autoComplete="new-password"
+                                autoCorrect="off"
+                                disabled={isLoading}
+                            />
+                            <Button
+                                variant="link"
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                                className="cursor-pointer absolute right-0 top-0"
+                                aria-label="Mostrar senha"
+                            >
+                                {passwordVisible ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </Button>
+                        </div>
+                        <div className='relative'>
+                            <Input
+                                required
+                                value={confirmPassword}
+                                onChange={e => setConfirmPassword(e.target.value)}
+                                id="password"
+                                placeholder="Confirmar senha"
+                                type={confirmPasswordVisible ? "text" : "password"} // Condicionando o tipo de input
+                                autoCapitalize="none"
+                                autoComplete="new-password"
+                                autoCorrect="off"
+                                disabled={isLoading}
+                            />
+                            <Button
+                                variant="link"
+                                type="button"
+                                onClick={toggleConfirmPasswordVisibility}
+                                className="absolute cursor-pointer right-0 top-0"
+                                aria-label="Mostrar senha"
+                            >
+                                {confirmPasswordVisible ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </Button>
+                        </div>
+
+                        <div className='mt-2 space-y-2'>
+                            {criteria.map(({ text, test }, index) => (
+                                <p key={index} className="flex items-center gap-2 text-sm">
+                                    {test(password) ? (
+                                        <CheckCircle2 className="text-primary-custom" size={16} />
+                                    ) : (
+                                        <XCircle className="text-red-500" size={16} />
+                                    )}
+                                    {text}
+                                </p>
+
+                            ))}
+                            <p className="flex items-center gap-2 text-sm ">
+                                {password && confirmPassword && password === confirmPassword ? (
+                                    <CheckCircle2 className="text-primary-custom" size={16} />
+                                ) : (
+                                    <XCircle className="text-red-500" size={16} />
+                                )}
+                                As senhas precisam coincidir
+                            </p>
+                        </div>
+
                     </div>
                     <Button
-                        disabled={isLoading}
+                        onClick={handleSubmit}
+                        disabled={!allValid}
                         className="cursor-pointer bg-primary-custom hover:bg-green-700 transition-colors duration-100 ease-in text-white"
                     >
                         {isLoading && <Loader className="mr-2 h-4 w-4 animate-spin" />}
